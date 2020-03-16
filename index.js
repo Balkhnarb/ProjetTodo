7a2a07388b65cd2a771d58ca5dd3bbdf5d17f161
Balkhnarb <yvancanal55@gmail.com>
2020-03-16 13:40:22 +0100

    "fin"

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 const express = require('express')
 const mysql = require('mysql2')
+var bodyParser = require("body-parser");
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
   host: 'db',
@@ -17,14 +21,14 @@ app.get('/todos',function(req,res){
 })
 
 app.post('/todos',function(req,res){
-  connection.query("INSERT INTO todo (label,isDone) VALUES ('?',?)",[req.body.label,req.body.isDone],function(err,result,fields){
-    res.json(result)
+  connection.query("INSERT INTO todo (label,isDone) VALUES (?,?)",[req.body.label,req.body.isDone],function(err,result,fields){
+    res.send(err+result+fields)
   })
 })
 
 app.put('/todos/:id',function(req,res){
-  connection.query("UPDATE todo SET label='?',isDone=? WHERE id = ?",[req.body.label,req.body.isDone,req.params.id],function(err,result,fields){
-    res.json(result)
+  connection.query("UPDATE todo SET label=?,isDone=? WHERE id = ?",[req.body.label,req.body.isDone,req.params.id],function(err,result,fields){
+    res.send(result)
   })
 })
 
